de7d179093084a3bb733cb16e1e6adb41ec94ba7
Puneeth-kmp <142174684+Puneeth-kmp@users.noreply.github.com>
2024-08-11 14:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import streamlit as st
 import io
 import re
-import plotly.graph_objects as go
 from collections import defaultdict
+import plotly.graph_objects as go
 import plotly.express as px
+import streamlit as st
 
 def extract_data(file):
     data = defaultdict(lambda: defaultdict(list))
